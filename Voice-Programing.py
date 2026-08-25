4a476ed2138d032a73f4915fb92e6d85a7efda10
Heeie <114531189+Heeie@users.noreply.github.com>
2026-08-26 00:22:01 +0100

    VoiceCode - Update UI/UX
    Add some new elements to the UI

--- Voice-Programing.py
+++ Voice-Programing.py
@@ -85,8 +85,8 @@ class Aplicação(ctk.CTk):
         self.aba_Principal = ctk.CTkTabview(self, width=400)
         self.aba_Principal.grid(row=0,column=1,sticky="nsew", padx=10, pady=10)
         self.aba_Principal.add("Principal")
-        self.aba_Principal.add("Preferencias")
-        self.aba_Principal.add("Configurações")
+        self.aba_Principal.add("Preferências")
+        self.aba_Principal.add("Sistema")
         
         #abas que vou criar
         self.abaLateral()
@@ -103,42 +103,100 @@ class Aplicação(ctk.CTk):
         
     
     def abaLateral(self):
-        self.titulo = ctk.CTkLabel(self.barra_Lateral, text="Selecione a linguagem de programação", font=ctk.CTkFont(size=15, weight="bold"))
-        
+        self.titulo = ctk.CTkLabel(self.barra_Lateral, text="VoiceCode", font=ctk.CTkFont(size=20, weight="bold"))
         self.titulo.pack(pady=(30,10), padx=(20,20))
         
         #Tipo de Linguagem
+        self.tipo_Linguagem = ctk.IntVar(value=0)
+        radiolabel = ctk.CTkLabel(self.barra_Lateral, text="Selecione a linguagem de programação")
+        self.radio_Python = ctk.CTkRadioButton(self.barra_Lateral, text="Python",
+                                               variable=self.tipo_Linguagem,
+                                               value=1)
         
-        
-       
+        self.radio_Java = ctk.CTkRadioButton(self.barra_Lateral, text="Java",
+                                               variable=self.tipo_Linguagem,
+                                               value=2)
+        radiolabel.pack()
+        self.radio_Python.pack(pady=10)
+        self.radio_Python.select()
+        self.radio_Java.pack(pady=10)
         
         self.switch_Tema = ctk.CTkSwitch(self.barra_Lateral, text="Modo escuro")
         self.switch_Tema.pack(pady=(10,10), side="bottom")
+        self.switch_Tema.select()
         
     def abaPrincipal(self):
         
         paginaPrincipal = self.aba_Principal.tab("Principal")
-        self.btn_Ouvir = ctk.CTkButton(self.barra_Lateral, text="Ouvir")
+        
+        texto="Ouvir"
+        self.btn_Ouvir = ctk.CTkButton(paginaPrincipal, text=texto,
+                                       width=200, height=40, 
+                                       fg_color="red", 
+                                       hover_color="darkred")
+        
         self.btn_Ouvir.pack(pady=(10,10))
         
-        self.btn_Parar = ctk.CTkButton(self.barra_Lateral, text="Para")
-        self.btn_Parar.pack(pady=(10,10)) 
-       
+        self.Text_Viewer = ctk.CTkLabel(paginaPrincipal, height=150, width= 200)
+        self.Text_Viewer.pack(pady=10,padx=10)
+
+        #self.btn_Parar = ctk.CTkButton(paginaPrincipal, text=texto)
+        #self.btn_Parar.pack(pady=(10,10)) 
     
         
     def abaPreferencias(self):
-        pass
+        paginaPreferencias = self.aba_Principal.tab("Preferências")
+        
+        label_Idiomas = ctk.CTkLabel( paginaPreferencias, text="Selecione o idioma:")
+        label_Idiomas.pack(pady=(20,10))
+        
+        menu_Idiomas = ctk.CTkOptionMenu( paginaPreferencias, values=["Português","Ingles","Espanhol"])
+        menu_Idiomas.pack(pady=(0,10))
+        
+        label_Volume = ctk.CTkLabel(paginaPreferencias , text="Volume do sistema:")
+        label_Volume.pack(pady=(10,5))
+        
+        self.slider_Volume = ctk.CTkSlider(paginaPreferencias, 
+                                           from_=0,
+                                           to=100)
+        self.slider_Volume.pack(pady=(20,10))
+        self.slider_Volume.set(50)
+        
+        label_Value_Volume = ctk.CTkLabel(paginaPreferencias, text="50%")
+        label_Value_Volume.pack()
             
     def abaSistema(self):
-        pass
+        paginaSistema = self.aba_Principal.tab("Sistema")
+        
+        label_Select_Micro = ctk.CTkLabel(paginaSistema , 
+                                          text="Selecione um microfone:",
+                                          font=ctk.CTkFont(size=15))
+        label_Select_Micro.pack(pady=(20,10))
+        
+        menu_Microfone = ctk.CTkOptionMenu( paginaSistema, 
+                                           values=["Microfone default",
+                                                   "microfone externo1",
+                                                   "Microfone externo2"])
+        menu_Microfone.pack(pady=(0,10))
+        
+        
+        label_Testar_Microfone = ctk.CTkLabel(paginaSistema,
+                                              text="Testar Microfone",
+                                              font=ctk.CTkFont(size=15))
+        label_Testar_Microfone.pack(pady=(10,10))
+        
+        
+        progress_Micro = ctk.CTkProgressBar(paginaSistema, width=500)
+        
+        progress_Micro.pack(pady=(10,20))
+        progress_Micro.set(0)
+        
+        btn_Testar_Micro = ctk.CTkButton(paginaSistema, 
+                                        text="Iniciar Teste", 
+                                        width=200,
+                                        height=50)
+        btn_Testar_Micro.pack()
         
         
 janela = Aplicação()
 janela.mainloop()
-
-#titulo = ctk.CTkLabel(text="Voice Code", font=Font(size=22, weight="bold", family="Arial"))
-#titulo.pack(pady=(50,50), padx=(5,10))
-
-#ctk.CTkButton(janela, text="Iniciar", command=iniciar).pack(pady=10)
-#ctk.CTkButton(janela, text="Parar", command=parar).pack(pady=10)
-
